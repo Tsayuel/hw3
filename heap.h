4d4791c14846423d6eea361096ccb111cfefe7a9
Samuel Wang <twang508@usc.edu>
2022-10-18 06:34:58 +0000

    All done

--- heap.h
+++ heap.h
@@ -2,6 +2,7 @@
 #define HEAP_H
 #include <functional>
 #include <stdexcept>
+#include <vector>
 
 template <typename T, typename PComparator = std::less<T> >
 class Heap
@@ -60,15 +61,38 @@ public:
   size_t size() const;
 
 private:
-  /// Add whatever helper functions and data members you need below
-
-
-
-
+  /// Add whatever helper functions and list members you need below
+	std::vector<T> list;
+	PComparator c;
+	int m;
+	void trickleUp(int idx);
+	void trickleDown(int idx);
+	
 };
 
 // Add implementation of member functions here
+template <typename T, typename PComparator>
+Heap<T,PComparator>::Heap(int m, PComparator c)
+{
+	m=m;
+	c=c;
+}
+template <typename T, typename PComparator>
+Heap<T,PComparator>::~Heap()
+{
 
+}
+template <typename T, typename PComparator>
+bool Heap<T,PComparator>::empty() const
+{
+	if (list.size()==0) return true;
+	return false;
+}
+template <typename T, typename PComparator>
+size_t Heap<T,PComparator>::size() const
+{
+	return list.size();
+}
 
 // We will start top() for you to handle the case of 
 // calling top on an empty heap
@@ -81,16 +105,86 @@ T const & Heap<T,PComparator>::top() const
     // ================================
     // throw the appropriate exception
     // ================================
-
-
+		throw std::underflow_error("Empty Heap");
   }
   // If we get here we know the heap has at least 1 item
   // Add code to return the top element
-
-
-
+	return list[0];
 }
 
+
+template <typename T, typename PComparator>
+void Heap<T,PComparator>::trickleUp(int idx)
+{
+	int parent=(idx-1)/m;
+	while(parent>=0 && c(list[idx], list[parent]))
+	{
+		T temp= list[idx];
+		list[idx]=list[parent];
+		list[parent]=temp;
+		idx=parent;
+		parent=(parent-1)/m;
+	}
+}
+
+template <typename T, typename PComparator>
+void Heap<T,PComparator>::push(const T& item)
+{
+	list.push_back(item);
+	trickleUp(list.size()-1);
+}
+
+
+template <typename T, typename PComparator>
+void Heap<T,PComparator>::trickleDown(int idx)
+{
+	int leaf = 0;
+	int pre = 0;
+	while(true)
+	{
+		if(leaf*m+1>int(list.size()-1)) break;
+		pre=leaf;
+		leaf=leaf*m+1;
+	}
+	if(idx>=leaf) return;
+	else if(idx>=pre)
+	{
+		while(pre<leaf)
+		{
+			bool b = false;
+			for(int i = 1; i<=m; i++)
+			{
+				if(pre*m+i<=int(list.size()-1))
+				{
+					b=true;
+					break;
+				}
+			}
+			if(!b)
+			{
+				if(idx==pre) return;
+			}
+			pre++;
+		}
+	}
+	T maxChild = list[idx*m+1];
+	int max_idx = idx*m+1;
+	for(int i = 2; i<=m; i++)
+	{
+		if(idx*m+i>int(list.size()-1)) break;
+		if(c(list[idx*m+i], maxChild))
+		{
+			maxChild = list[idx*m+i];
+			max_idx=idx*m+i;
+		}
+	}
+	if(c(maxChild, list[idx])) 
+	{
+		list[max_idx] = list[idx];
+		list[idx] = maxChild;
+		trickleDown(max_idx);
+	}
+}
 
 // We will start pop() for you to handle the case of 
 // calling top on an empty heap
@@ -101,15 +195,13 @@ void Heap<T,PComparator>::pop()
     // ================================
     // throw the appropriate exception
     // ================================
-
-
+		throw std::underflow_error("Empty Heap");
   }
-
-
-
+	list[0] = list[list.size()-1];
+	list.pop_back();
+	trickleDown(0);
 }
 
 
 
 #endif
-
--- logicsim/event.h
+++ logicsim/event.h
@@ -11,6 +11,10 @@ struct Event
 
 typedef struct EventLess {
         //write the operator() required to make this a functor that compares Events by time
+    bool operator()(Event* eva, Event* evb)
+    {
+        return eva->time < evb->time;
+    }
 } EventLess;
 	
 #endif
